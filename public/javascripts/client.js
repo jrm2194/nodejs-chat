fa06b839b15acce443bdfd62b625ddbb60378a9e
Jean <jean.meyer2@gmail.com>
2014-05-19 12:49:48 -0400

    adapted for heroku test

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,5 @@
 // connect to the socket server
-var socket = io.connect(); 
+var socket = io.connect("http://mysterious-sea-2318.herokuapp.com/"); 
 
 // if we get an "info" emit from the socket server then console.log the data we recive
 socket.on('info-123', function (response) {
